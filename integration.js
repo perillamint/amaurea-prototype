a630485def87cc57cea2abc3f2363fb6d271dbc2
perillamint <perillamint@quendi.moe>
2017-09-19 17:28:05 +0900

    Better integration code

--- integration.js
+++ integration.js
@@ -3,10 +3,11 @@
 // Use composite simpson rule here
 function integration(from, to, step, tbl) {
     let ret = 0;
-    for(let i = from + step * 2; i < from + (to - from) / 2; i += step) {
-        let x1 = from + step * (2 * ((i - from) / step - 2));
-        let x2 = from + step * (2 * ((i - from) / step - 1));
-        let x3 = from + step * (2 * ((i - from) / step));
+    let i = 0;
+    for(i = 2; i < (to-from) / step / 2; i++) {
+        let x1 = from + (2 * i - 2) * step;
+        let x2 = from + (2 * i - 1) * step;
+        let x3 = from + (2 * i) * step;
 
         ret += get_value_from_tbl(tbl, x1);
         ret += 4 * get_value_from_tbl(tbl, x2);
